4d00733e6c55d15e9a5146793a9c90d32f996953
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-09-12 15:28:35 +0200

    First commit (template from lesson)

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import { greeting } from "./greeting.js";
+
+console.log(greeting);
+
+import spaceImage from "./maybe.png";
+
+const image = document.createElement("img");
+image.src = spaceImage;
+
+document.body.appendChild(image);
+
+document.body.appendChild(image);
